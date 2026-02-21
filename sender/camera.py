9b159b844b9c35aa69a1c8385780a539711ca820
pathsandesh-sketch <pathsandesh@gmail.com>
2026-02-21 11:45:15 +0530

    Create camera.py

---
+++ sender/camera.py
@@ -0,0 +1,14 @@
+import cv2
+
+class Camera:
+    def __init__(self, index=0):
+        self.cap = cv2.VideoCapture(index)
+
+    def get_frame(self):
+        ret, frame = self.cap.read()
+        if not ret:
+            return None
+        return frame
+
+    def release(self):
+        self.cap.release()
